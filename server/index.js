c2f6dc0c37e355e0cffd462001eb76c8c153e7e2
itqan solutions <info@itqansolutions.org>
2025-11-30 21:31:50 +0200

    Fix server to listen in production

--- server/index.js
+++ server/index.js
@@ -29,8 +29,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
